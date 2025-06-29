2aa5682c2cf7bf3b99a8bc56d70bfb00a584b645
Crush2161 <s2595229@gmail.com>
2025-06-29 18:29:06 +0530

    Update ~reaction.py

--- PratikBots/modules/~reaction.py
+++ PratikBots/modules/~reaction.py
@@ -3,12 +3,12 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from PratikBots import PratikBots
 
-# List of reactions
-reactions = ["👍", "👎", "❤️", "😂", "🤯", "😮", "🤔", "😢", "🔥", "❤️‍🔥", "🕊️", "🥰", "🗿", "💯", "😎"]
+# List of reactions (only valid Telegram reactions)
+reactions = ["🥰", "❤️", "😁", "💋", "😱", "🤣", "😘", "❤️‍🔥", "👌", "🫡", "😍"]
 
 
 @PratikBots.on_message(filters.text)
 async def react_to_message(client: Client, message: Message):
     emoji = random.choice(reactions)
     await message.react(emoji)
-    
+
