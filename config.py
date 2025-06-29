37a2ecb52d412fc57390e5670d71953362f48836
Crush2161 <s2595229@gmail.com>
2025-06-29 18:38:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", None))
+API_ID = "27139681"
 # -------------------------------------------------------------
-BOT_TOKEN = getenv("API_HASH", None)
+API_HASH = "476018a0d8c7bce26eff25dd6f93b951"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
@@ -14,4 +14,3 @@ OWNER_ID = int(getenv("OWNER_ID", "2047173815"))
 SUPPORT_GRP = "https://t.me/+Rv1BIrd2MsgxMWVl"
 UPDATE_CHNL = "CRUSH_FOREVER"
 OWNER_USERNAME = "FOREVER_CRUSH"
-    
