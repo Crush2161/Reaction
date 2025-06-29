1bbd325147be3e92019ca8d921cb7f9f99529f05
Crush2161 <s2595229@gmail.com>
2025-06-29 18:47:59 +0530

    Update Clone.py

--- PratikBots/mplugin/Clone.py
+++ PratikBots/mplugin/Clone.py
@@ -74,7 +74,7 @@ async def clone_txt(client, message):
         except BaseException as e:
             logging.exception("Error while cloning bot.")
             await mi.edit_text(
-                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @ll_ALPHA_BABY_lll for assistance**"
+                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @FOREVER_CRUSH for assistance**"
             )
     else:
         await message.reply_text("**Provide Bot Token after /clone Command from @Botfather.**\n\n**Example:** `/clone bot token paste here`")
